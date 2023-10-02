cc2a343ca645cc08e6d21d94e4b11751bf564b47
R0hanBri770 <rohanbrittodvlp@gmail.com>
2023-10-03 00:57:16 +0530

    changed card designs

--- src/components/cards-to.tsx
+++ src/components/cards-to.tsx
@@ -1,22 +1,26 @@
 import { TaskToType } from "@/types/tasktotype";
 import React from "react";
+import { FormatDate } from "@/utilities/utillities";
+
 type Props = { assigned: TaskToType };
 const CardsTo = (props: Props) => {
   const { assigned } = props;
+  const date = new Date(assigned.task.deadline);
+  const deadline = FormatDate(date);
   return (
     <div>
-      <div className="py-10 px-5 border-2 rounded-xl min-h-full">
+      <div className="py-6 px-7 border-2 rounded-lg min-h-full">
         <div>
           <div className="flex flex-col gap-4">
             <div>
-              <p className="text-2xl">{assigned.task.name}</p>
-              <p className="mt-2">
+              <p className="font-medium">
                 To : <span className="text-[#3e38f5]">{assigned.to}</span>
               </p>
+              <p className="text-2xl font-medium h-14 my-3">{assigned.task.name}</p>
             </div>
             <div>
               <p>{assigned.task.description}</p>
-              <p>Due: {assigned.task.deadline}</p>
+              <p className="font-medium">Due : {deadline}</p>
             </div>
           </div>
         </div>
