539518b105114a37422be8b9d7586616f3f3db8b
Abirami Muthukumarasamy <109137044+artist-who-codes@users.noreply.github.com>
2023-10-02 19:42:29 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,10 @@ import { useEffect } from "react";
 import { getSession, signOut } from "@/server/auth/auth-user";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
+import HOD from "@/components/hod";
+import Staff from "@/components/staff";
+import Student from "@/components/student";
+import Principal from "@/components/principal";
 
 
 export default function Home() {
@@ -21,10 +25,29 @@ export default function Home() {
     },[])
     const mail = useSelector((state: RootState) => state.auth.email);
     
+
+    let pageComponent;
+  switch (mail) {
+    case 'hod@bhc.edu.in':
+      pageComponent = <HOD />;
+      break;
+    case 'staff@bhc.edu.in':
+      pageComponent = <Staff />;
+      break;
+    case 'cs215114102@bhc.edu.in':
+      pageComponent = <Student />;
+      break;
+    case 'principal@bhc.edu.in':
+      pageComponent = <Principal />;
+      break;
+    default:
+      // Handle cases where 'page' doesn't match any known page
+      pageComponent = <div>Page not found</div>;
+  }
+    
     return (
-        <>
-            <Dashboard/>
-           
+        <>  
+        {pageComponent}
         </>
     )
 }
