8741e22ac4e28a3316b10759ee31eaaf77937f41
fosslover <fosslover@duck.com>
2023-11-07 00:00:23 +0530

    HeberDashboard: Homepage: Fix UI Re-rendering issue
    Since the useState was an object the object reference won't change. Hence ending in UI not re-rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,121 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-import { getSession, signOut } from "@/server/auth/auth-user";
-import { createTask, fetchTasks, fetchUsers } from "@/server/data/fetch-data";
-import Dashboard from "@/components/dashboard";
+import { useEffect, useState } from "react";
+import { getSession } from "@/server/auth/auth-user";
+import { fetchTasks, fetchUsers } from "@/server/data/fetch-data";
 import { ProfileType } from "@/types/profiletype";
+import { ToLocalTime } from "@/utilities/utillities";
+import { Button, Skeleton } from "@nextui-org/react";
+import TaskForm from "@/components/task-form";
+import Image from "next/image";
+import exportIcon from "../../public/export.svg";
 
 export default function Home() {
-    const router = useRouter();
-    const id = useRef('')
-    const [user,setUser] = useState({id:'', name:'', role:'',role_power:0})
-    const [task, setTask]= useState({})
-    useEffect(()=>{
-        async function fetchSession(){
-    //      const result = createTask('a618b2fe-e563-4d08-9bfa-b6a0c82909b9', 'd636502c-8daf-412f-9aa0-718256f7c366', "Hybernetix Fees", "Collect the fees fir symposium and submit it", new Date(),"MileStone 1, MileStone2, MileStone3", "Milestone3");
-    //      console.log(result)
-            const data = await getSession()
-            if(data.data==null){
-                router.push('/login')
-            }
-            else{
-                id.current = data.data?.user.id
-                console.log(id.current)
-                const tasks = await fetchTasks(id.current);
-                const res = await fetchUsers()
-                if(tasks.data) {
-                    setTask(tasks.data)
-                }
-                const users = res.data as ProfileType[]
-                if(users!=null) var User = users.find(user=>user.id==userId)
-                console.log(users)
-                if(User!=undefined) setUser(User)
-            }
+  const router = useRouter();
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+  const [rolePower, setRolePower] = useState(0);
+  const date = new Date();
+  const today = ToLocalTime(date);
+  const Hour = today.getUTCHours();
+  var greeting: string;
+
+  if (Hour >= 0 && Hour < 12) {
+    greeting = "Good Morning";
+  } else if (Hour > 12 && Hour < 17) {
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Evening";
+  }
+
+  const [task, setTask] = useState({});
+  useEffect(() => {
+    async function fetchSession() {
+      //      const result = createTask('a618b2fe-e563-4d08-9bfa-b6a0c82909b9', 'd636502c-8daf-412f-9aa0-718256f7c366', "Hybernetix Fees", "Collect the fees fir symposium and submit it", new Date(),"MileStone 1, MileStone2, MileStone3", "Milestone3");
+      //      console.log(result)
+      const data = await getSession();
+      if (data.data == null) {
+        router.push("/login");
+      } else {
+        let id = data.data?.user.id;
+        console.log(id);
+        const tasks = await fetchTasks(id);
+        const res = await fetchUsers();
+        if (tasks.data) {
+          setTask(tasks.data);
         }
-        fetchSession()
-    },[])
+        const users = res.data as ProfileType[];
+        if (users) {
+          let User = users.find((user) => user.id === id);
+          if (User) {
+            setName(User.name);
+            setId(User.id);
+            setRole(User.role);
+            setRolePower(User.role_power);
+          }
+        }
+      }
+    }
+    fetchSession();
+  }, [name]);
 
-    
-    let userId = id.current as unknown as string
-
-  console.log(user,"PAGE")
-  console.log(task)
-    
-    return (
-        <div>
-            <Dashboard user={user}/>
-
+  return (
+    <>
+      {rolePower === 1 ? (
+        <div className="px-20 py-7 flex justify-between items-center">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-4xl font-semibold">
+              {greeting}
+              {name ? (
+                <span className="text-[rgba(62,56,245)]"> {name}</span>
+              ) : (
+                <Skeleton className="h-3 w-3/5 rounded-lg" />
+              )}
+              !
+            </h1>
+            <p className="text-xl  text-slate-500 mt-1">
+              Here is a list of your tasks
+            </p>
+          </div>
+          <div className="flex gap-12  items-center justify-center">
+            <Button className="rounded-lg bg-[rgba(62,56,245,0.9)] text-white font-medium">
+              Sign-out
+            </Button>
+          </div>
         </div>
-  
-    )
+      ) : (
+        <div className="px-20 py-7 flex justify-between items-center">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-4xl font-semibold flex gap-2">
+              {greeting}
+              {name ? (
+                <span className="text-[rgba(62,56,245)]"> {name}</span>
+              ) : (
+                <span>
+                  <Skeleton className="h-12 w-40 rounded-lg" />
+                </span>
+              )}
+              !
+            </h1>
+            <p className="text-xl text-slate-500 mt-1">
+              Here is a list of your tasks
+            </p>
+          </div>
+          {name ? (
+            <div className="flex gap-12  items-center justify-center">
+              <TaskForm />
+              <Button className="rounded-lg bg-slate-100">
+                <Image src={exportIcon} width={20} alt="Plus"></Image>
+                Export
+              </Button>
+              <Button className="rounded-lg bg-[rgba(62,56,245,0.9)] text-white font-medium">
+                Sign-out
+              </Button>
+            </div>
+          ) : null}
+        </div>
+      )}
+    </>
+  );
 }
