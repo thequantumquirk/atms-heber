543b26d967c7285236407b6195e5d24438156060
Abirami Muthukumarasamy <109137044+A-beginner-2114@users.noreply.github.com>
2023-11-30 17:11:34 +0530

    Fixing display of completion

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -34,8 +34,7 @@ const LoginPage = () => {
         style={{ backgroundImage: "url(./Login/circle-scatter.svg)" }}
       >
         <h2 className="text-4xl text-center font-semibold">
-          Welcome to{" "}
-          <span className="text-[hsl(242,80%,60%)]">ATMS</span>
+          Welcome to <span className="text-[hsl(242,80%,60%)]">ATMS</span>
         </h2>
         <div className="md:w-[30vw] p-8 flex flex-col gap-5">
           <h1 className="text-3xl font-medium mb-2">Login</h1>
@@ -51,6 +50,7 @@ const LoginPage = () => {
               }}
             />
           </div>
+
           <div>
             <label className="text-lg font-medium">Password</label>
             <input
--- src/components/cards-from.tsx
+++ src/components/cards-from.tsx
@@ -1,83 +1,32 @@
+import React, { useState } from "react";
 import { Tasktype } from "@/types/tasktype";
 import { FormatDate } from "@/utilities/utillities";
 import Progress from "./progress";
-import NoTask from "../../public/notask.svg"
-import Image from 'next/image';
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Button } from "@nextui-org/react";
-import {CircularProgress} from "@nextui-org/react";
+import NoTask from "../../public/notask.svg";
+import Image from "next/image";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  CircularProgress,
+} from "@nextui-org/react";
+
+function stripMilestones(status_details: string, current_status: string) {
+  const completedMilestones: string[] = current_status
+    .split(",")
+    .map((milestone) => milestone.trim());
+  return completedMilestones;
+}
 
 type Props = { assigned: Tasktype[] | undefined };
 
-const CardsFrom = ({assigned}: Props) => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const handleOpen = () => {
-        onOpen();
-    } 
-    function stripMilestones(status_details:string, current_status:string){
-        
-        const completedMilestones: string[] = current_status.split(',').map((milestone) => milestone.trim());
-        return completedMilestones
-    }  
-    if(assigned){
-    return (
-      <>
-        {assigned.length != 0 ? (
-          <div className="grid grid-cols-3 gap-5 my-7">
-              {assigned.map((task, key)=>{
-                  const date = FormatDate(task.task_due)
-                  const complete = Math.ceil(task.current_status.length*100/task.status_details.length)
-                  const completedMilestones = stripMilestones(task.status_details, task.current_status)
-                  const milestones: string[] = task.status_details.split(',');
-                  return(
-                    
-                      <div key={key} className="bg-[#3e38f5]/10 py-5 rounded">
-                        <Button onPress={() => handleOpen()} className='w-full h-full bg-[#000000]/0'>
-                      <div className='w-full'>
-                          <div className=" w-full flex justify-between leading-10">
-                              <div className="py-1 h-14 text-left">
-                                  <p className="text-xl font-bold">{task.task_title}</p>
-                                  <p className="text-sm text-[#a7a9d2]">{task.task_description}</p>
-                                  <p>Assigned To : <span className="text-[#3e38f5] font-semibold">{task.assignee_name.name}</span></p>
-                              </div>
-                              <div>
-                              <Progress percent={complete}/> 
-                              </div>
-                          </div>
-                          <div className="grid grid-flow-col justify-stretch pt-5 w-full gap-5">
-                              <Button className="font-medium bg-indigo-600/10 py-1 px-3 rounded ring-1 ring-inset ring-indigo-600/50 text-center">{date}</Button>
-                          </div>
-                      </div>
-                      </Button>
-         <Modal size="xl" isOpen={isOpen} onClose={onClose}>
-         <ModalContent>
-             <ModalHeader className="flex flex-col gap-1 text-xl">Task Completion</ModalHeader>
-                 <ModalBody>
-                 {milestones.map((milestone, key) => (
-                        <p key={key} className='my-1 mx-16'>
-                                <span className={`p-2 rounded ${completedMilestones.includes(milestone.trim()) ? 'bg-green-200' : 'bg-red-200'}`}>{milestone.trim()}</span>
-                        </p>
-                    ))}
-                 </ModalBody>
-                 <ModalFooter>                                        
-                     <Button className="rounded bg-[rgba(62,56,245,0.9)] text-white font-medium" onPress={() => onClose()}>Close</Button>
-                 </ModalFooter>
-          </ModalContent>
-         </Modal>
-                  </div>
-              )})}
-          
-        </div>
-        ):(
-          <div className="text-center">
-            <Image src={NoTask} alt="NoTask" className="mx-auto"></Image>
-            <p className=" text-stone-400 text-sm -my-10 ">
-              No one to monitor at the moment
-            </p>
-          </div>
-        )}
-      </>
-    );
-  } else {
+const CardsFrom = ({ assigned }: Props) => {
+  const [openModalIndex, setOpenModalIndex] = useState<number | null>(null);
+
+  if (!assigned) {
     return (
       <div className="my-14">
         <CircularProgress
@@ -95,6 +44,104 @@ const CardsFrom = ({assigned}: Props) => {
       </div>
     );
   }
+
+  const handleOpenModal = (index: number) => {
+    setOpenModalIndex(index);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModalIndex(null);
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-5 my-7">
+        {assigned.map((task, key) => {
+          const date = FormatDate(task.task_due);
+          const completedMilestones = stripMilestones(
+            task.status_details,
+            task.current_status
+          );
+          const milestones: string[] = task.status_details.split(",");
+          let complete = 0;
+          if (task.current_status.length !== 0) {
+            complete = Math.ceil(
+              (completedMilestones.length * 100) / milestones.length
+            );
+          }
+
+          const isModalOpen = openModalIndex === key;
+          return (
+            <div key={key} className="bg-[#3e38f5]/10 py-5 rounded">
+              <Button
+                onPress={() => handleOpenModal(key)} // Pass 'key' here
+                className="w-full h-full bg-[#000000]/0"
+              >
+                <div className="w-full">
+                  {
+                    <div className="w-full">
+                      <div className=" w-full flex justify-between leading-10">
+                        <div className="py-1 h-14 text-left">
+                          <p className="text-xl font-bold">{task.task_title}</p>
+                          <p className="text-sm text-[#a7a9d2]">
+                            {task.task_description}
+                          </p>
+                          <p>
+                            Assigned To :{" "}
+                            <span className="text-[#3e38f5] font-semibold">
+                              {task.assignee_name.name}
+                            </span>
+                          </p>
+                        </div>
+                        <div>
+                          <Progress percent={complete} />
+                        </div>
+                      </div>
+                      <div className="grid grid-flow-col justify-stretch pt-5 w-full gap-5">
+                        <Button className="font-medium bg-indigo-600/10 py-1 px-3 rounded ring-1 ring-inset ring-indigo-600/50 text-center">
+                          {date}
+                        </Button>
+                      </div>
+                    </div>
+                  }
+                </div>
+              </Button>
+              <Modal size="xl" isOpen={isModalOpen} onClose={handleCloseModal}>
+                <ModalContent>
+                  <ModalHeader className="flex flex-col gap-1 text-xl">
+                    Task Completion
+                  </ModalHeader>
+                  <ModalBody>
+                    {milestones.map((milestone, index) => (
+                      <p key={index} className="my-1 mx-16">
+                        <span
+                          className={`p-2 rounded ${
+                            completedMilestones.includes(milestone.trim())
+                              ? "bg-green-200"
+                              : "bg-red-200"
+                          }`}
+                        >
+                          {milestone.trim()}
+                        </span>
+                      </p>
+                    ))}
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button
+                      className="rounded bg-[rgba(62,56,245,0.9)] text-white font-medium"
+                      onPress={handleCloseModal}
+                    >
+                      Close
+                    </Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default CardsFrom;
