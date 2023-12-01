ff79f57cf94b11e67c28a456f73eef4900555f6b
Abirami Muthukumarasamy <109137044+A-beginner-2114@users.noreply.github.com>
2023-12-01 11:28:48 +0530

    No Task Update

--- src/components/cards-from.tsx
+++ src/components/cards-from.tsx
@@ -60,113 +60,124 @@ const CardsFrom = ({ Assigned, onDelete }: Props) => {
       setOpenModalIndex(null);
     };
 
-    return (
-      <>
-        <div className="grid grid-cols-3 gap-5 my-7">
-          {assigned.map((task, key) => {
-            const date = FormatDate(task.task_due);
-            const completedMilestones = stripMilestones(
-              task.status_details,
-              task.current_status
-            );
-            const milestones: string[] = task.status_details.split(",");
-            let complete = 0;
-            if (task.current_status.length !== 0) {
-              complete = Math.ceil(
-                (completedMilestones.length * 100) / milestones.length
+    if (assigned.length != 0) {
+      return (
+        <>
+          <div className="grid grid-cols-3 gap-5 my-7">
+            {assigned.map((task, key) => {
+              const date = FormatDate(task.task_due);
+              const completedMilestones = stripMilestones(
+                task.status_details,
+                task.current_status
               );
-            }
+              const milestones: string[] = task.status_details.split(",");
+              let complete = 0;
+              if (task.current_status.length !== 0) {
+                complete = Math.ceil(
+                  (completedMilestones.length * 100) / milestones.length
+                );
+              }
 
-            const isModalOpen = openModalIndex === key;
-            return (
-              <div
-                key={key}
-                className="bg-[#3e38f5]/10 py-5 rounded hover:bg-[#3e38f5]/20"
-              >
-                <Button
-                  onPress={() => handleOpenModal(key)} // Pass 'key' here
-                  className="w-full h-full bg-[#000000]/0"
+              const isModalOpen = openModalIndex === key;
+              return (
+                <div
+                  key={key}
+                  className="bg-[#3e38f5]/10 py-5 rounded hover:bg-[#3e38f5]/20"
                 >
-                  <div className="w-full">
-                    {
-                      <div className="w-full">
-                        <div className=" w-full flex justify-between leading-10">
-                          <div className="py-1 h-14 text-left">
-                            <p className="text-xl font-bold">
-                              {task.task_title}
-                            </p>
-                            <p className="text-sm text-[#a7a9d2]">
-                              {task.task_description}
-                            </p>
-                            <p>
-                              Assigned To :{" "}
-                              <span className="text-[#3e38f5] font-semibold">
-                                {task.assignee_name.name}
-                              </span>
-                            </p>
+                  <Button
+                    onPress={() => handleOpenModal(key)} // Pass 'key' here
+                    className="w-full h-full bg-[#000000]/0"
+                  >
+                    <div className="w-full">
+                      {
+                        <div className="w-full">
+                          <div className=" w-full flex justify-between leading-10">
+                            <div className="py-1 h-14 text-left">
+                              <p className="text-xl font-bold">
+                                {task.task_title}
+                              </p>
+                              <p className="text-sm text-[#a7a9d2]">
+                                {task.task_description}
+                              </p>
+                              <p>
+                                Assigned To :{" "}
+                                <span className="text-[#3e38f5] font-semibold">
+                                  {task.assignee_name.name}
+                                </span>
+                              </p>
+                            </div>
+                            <div>
+                              <Progress percent={complete} />
+                            </div>
                           </div>
-                          <div>
-                            <Progress percent={complete} />
+                          <div className="grid grid-flow-col justify-stretch pt-5 w-full gap-5">
+                            <div className="font-medium bg-indigo-600/10 py-1 px-3 rounded ring-1 ring-inset ring-indigo-600/50 text-center pt-2.5">
+                              {date}
+                            </div>
+                            <Button
+                              className="bg-indigo-500 hover:bg-red-500 py-1 px-3 rounded text-white text-center font-semibold"
+                              onClick={() => {
+                                deleteTask(task.id);
+                                onDelete();
+                              }}
+                            >
+                              Delete Task
+                            </Button>
                           </div>
                         </div>
-                        <div className="grid grid-flow-col justify-stretch pt-5 w-full gap-5">
-                          <div className="font-medium bg-indigo-600/10 py-1 px-3 rounded ring-1 ring-inset ring-indigo-600/50 text-center pt-2.5">
-                            {date}
-                          </div>
-                          <Button
-                            className="bg-indigo-500 hover:bg-red-500 py-1 px-3 rounded text-white text-center font-semibold"
-                            onClick={() => {
-                              deleteTask(task.id);
-                              onDelete();
-                            }}
-                          >
-                            Delete Task
-                          </Button>
-                        </div>
-                      </div>
-                    }
-                  </div>
-                </Button>
-                <Modal
-                  size="xl"
-                  isOpen={isModalOpen}
-                  onClose={handleCloseModal}
-                >
-                  <ModalContent>
-                    <ModalHeader className="flex flex-col gap-1 text-xl">
-                      Task Completion
-                    </ModalHeader>
-                    <ModalBody>
-                      {milestones.map((milestone, index) => (
-                        <p key={index} className="my-1 mx-16">
-                          <span
-                            className={`p-2 rounded ${
-                              completedMilestones.includes(milestone.trim())
-                                ? "bg-green-200"
-                                : "bg-red-200"
-                            }`}
-                          >
-                            {milestone.trim()}
-                          </span>
-                        </p>
-                      ))}
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button
-                        className="rounded bg-[rgba(62,56,245,0.9)] text-white font-medium"
-                        onPress={handleCloseModal}
-                      >
-                        Close
-                      </Button>
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
-              </div>
-            );
-          })}
+                      }
+                    </div>
+                  </Button>
+                  <Modal
+                    size="xl"
+                    isOpen={isModalOpen}
+                    onClose={handleCloseModal}
+                  >
+                    <ModalContent>
+                      <ModalHeader className="flex flex-col gap-1 text-xl">
+                        Task Completion
+                      </ModalHeader>
+                      <ModalBody>
+                        {milestones.map((milestone, index) => (
+                          <p key={index} className="my-1 mx-16">
+                            <span
+                              className={`p-2 rounded ${
+                                completedMilestones.includes(milestone.trim())
+                                  ? "bg-green-200"
+                                  : "bg-red-200"
+                              }`}
+                            >
+                              {milestone.trim()}
+                            </span>
+                          </p>
+                        ))}
+                      </ModalBody>
+                      <ModalFooter>
+                        <Button
+                          className="rounded bg-[rgba(62,56,245,0.9)] text-white font-medium"
+                          onPress={handleCloseModal}
+                        >
+                          Close
+                        </Button>
+                      </ModalFooter>
+                    </ModalContent>
+                  </Modal>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <div className="text-center">
+          <Image src={NoTask} alt="NoTask" className="mx-auto"></Image>
+          <p className=" text-stone-400 text-sm -my-10 ">
+            No tasks assigned for you at the moment
+          </p>
         </div>
-      </>
-    );
+      );
+    }
   } else {
     return (
       <div className="my-14">
